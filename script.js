366572c8e595a2066b7b43e41be4e3c9b940b086
revoladesign <revola204@gmail.com>
2025-05-10 00:29:34 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function openPopup() {
+  document.getElementById("signup-popup").style.display = "flex";
+}
+
+function closePopup() {
+  document.getElementById("signup-popup").style.display = "none";
+}
