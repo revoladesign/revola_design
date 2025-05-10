258d0cbe50f1d1038853a25f053e279a8fac8238
revoladesign <revola204@gmail.com>
2025-05-10 11:24:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ function openPopup() {
 function closePopup() {
   document.getElementById("signup-popup").style.display = "none";
 }
+
+function toggleSignup(btn) {
+  const message = document.querySelector('.signup-message');
+  message.style.display = "block";
+  message.textContent = `Sign up for ${btn.textContent}`;
+}
