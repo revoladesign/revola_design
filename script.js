986a65503c1584c92943906cca8e45caf6ebd01e
revoladesign <revola204@gmail.com>
2025-05-10 12:39:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-function openPopup() {
-  document.getElementById("signup-popup").style.display = "flex";
-}
-
-function closePopup() {
-  document.getElementById("signup-popup").style.display = "none";
-}
-
+<script>
+    // To show the "Sign up" under Services
+    document.querySelectorAll('li').forEach(item => {
+        if (item.textContent === 'Service') {
+            item.addEventListener('click', () => {
+                alert('Sign up');
+            });
+        }
+    });
+</script>
 function toggleSignup(btn) {
   const message = document.querySelector('.signup-message');
   message.style.display = "block";
